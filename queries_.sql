90dd2fafdacf57409235c8b952534b090a2832a9
Khalid Kadri <115176206+kmkadri@users.noreply.github.com>
2024-08-27 22:17:53 +0100

    Add files via upload

---
+++ queries_.sql
@@ -0,0 +1,18 @@
+-- Query to extract all columns from Salesorderheader table:
+
+SELECT 
+	* 
+FROM `tc-da-1.adwentureworks_db.salesorderheader`
+
+-- Query for Regions Table:
+
+SELECT 
+    DISTINCT CustomerID,
+    territory.Group,
+    Name AS Region,
+    CountryRegionCode AS Country,
+FROM `tc-da-1.adwentureworks_db.salesorderheader` soh
+JOIN `adwentureworks_db.salesterritory` as territory ON soh.TerritoryID = territory.TerritoryID
+
+
+
